2860e8a0bd0b4bea149db1c2f55fdd75686e6894
Moskize91 <i@taozeyu.com>
2019-02-22 17:03:44 +0800

    refactor: split into 1.0 & 2.0

--- quickStart/2.0/index.js
+++ quickStart/2.0/index.js
@@ -1,14 +1,14 @@
 var whiteWebSdk = new WhiteWebSdk();
 var miniToken = 'WHITEcGFydG5lcl9pZD1DYzlFNTJhTVFhUU5TYmlHNWJjbkpmVThTNGlNVXlJVUNwdFAmc2lnPTE3Y2ZiYzg0ZGM5N2FkNDAxZmY1MTM0ODMxYTdhZTE2ZGQ3MTdmZjI6YWRtaW5JZD00JnJvbGU9bWluaSZleHBpcmVfdGltZT0xNTY2MDQwNjk4JmFrPUNjOUU1MmFNUWFRTlNiaUc1YmNuSmZVOFM0aU1VeUlVQ3B0UCZjcmVhdGVfdGltZT0xNTM0NDgzNzQ2Jm5vbmNlPTE1MzQ0ODM3NDYzMzYwMA';
 
-var url = 'https://cloudcapiv3.herewhite.com/room?token=' + miniToken;
+var url = 'https://cloudcapiv4.herewhite.com/room?token=' + miniToken;
 var requestInit = {
     method: 'POST',
     headers: {
         "content-type": "application/json",
     },
     body: JSON.stringify({
-        name: 'my whiteboard',
+        name: '我的第一个 White 房间',
         limit: 100, // 房间人数限制
     }),
 };
